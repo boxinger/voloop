4577ecaf7561ccd87954676391373045813ea53a
boxing <202044743+boxinger@users.noreply.github.com>
2026-05-07 21:40:23 +0800

    修复include语法问题

--- bsp/stm32g4/Inc/voloop_stm32.h
+++ bsp/stm32g4/Inc/voloop_stm32.h
@@ -1,7 +1,7 @@
 #ifndef VOLOOP_STM32_H
 #define VOLOOP_STM32_H
 
-#include voloop_stm32_oledgfx.h
-#include voloop_stm32_oledll.h
+#include "voloop_stm32_oledgfx.h"
+#include "voloop_stm32_oledll.h"
 
 #endif /* VOLOOP_STM32_H */
