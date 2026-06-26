6130dc46456807bc84c0c6ca9d55a0c5dd887879
boxing <202044743+boxinger@users.noreply.github.com>
2026-06-26 23:38:06 +0800

    revert(bsp)：移除bsp库

--- bsp/stm32g4/Inc/voloop_stm32.h
+++
@@ -1,9 +0,0 @@
-#ifndef VOLOOP_STM32_H
-#define VOLOOP_STM32_H
-
-#include "../../common/voloop_bsp_def.h"
-#include "voloop_stm32_oledgfx.h"
-#include "voloop_stm32_oledll.h"
-#include "voloop_stm32_encoder.h"
-
-#endif /* VOLOOP_STM32_H */
