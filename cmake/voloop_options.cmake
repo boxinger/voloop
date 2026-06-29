d78391408a490e31742a19f39862309646c5310b
boxing <202044743+boxinger@users.noreply.github.com>
2026-06-30 01:56:59 +0800

    feat(cmake)：更改构建体系

---
+++ cmake/voloop_options.cmake
@@ -0,0 +1,4 @@
+option(VOLOOP_BUILD_TESTS
+    "Build voloop tests"
+    ${PROJECT_IS_TOP_LEVEL}
+)
---
+++ cmake/voloop_testing.cmake
@@ -0,0 +1,18 @@
+function(voloop_add_c_test name)
+    add_executable(${name} ${ARGN})
+
+    target_link_libraries(${name}
+        PRIVATE
+            voloop::voloop
+    )
+
+    target_compile_features(${name}
+        PRIVATE
+            c_std_99
+    )
+
+    add_test(
+        NAME ${name}
+        COMMAND ${name}
+    )
+endfunction()
